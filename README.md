2e3ea3e47230778c1565896454940de9a22c6745
Eimantas <eimantas.stonys@gmail.com>
2014-12-14 19:01:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+![alt tag](http://i.imgur.com/cExEbvs.png)
+
+
+Rhinoapp will help you to organize your freelancing tasks
---
+++ rhinoapp/settings.py
@@ -0,0 +1,101 @@
+"""
+Django settings for rhinoapp project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'phhe-)va7mqrw$=x74@(h$%dc07nuxq^=0e%g999o4*)vtf)&y'
+
+
+# Facebook API settings
+FACEBOOK_APP_ID = '1502872819925967'
+FACEBOOK_APP_SECRET = 'bc817aa931b8a27eeed53a0d1acb4767'
+FACEBOOK_STORE_LIKES = False
+FACEBOOK_STORE_FRIENDS = False
+
+AUTH_USER_MODEL = 'django_facebook.FacebookCustomUser'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates/'),
+)
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django_facebook',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'rhinoapp.urls'
+
+WSGI_APPLICATION = 'rhinoapp.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
